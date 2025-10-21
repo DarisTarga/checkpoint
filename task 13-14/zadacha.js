9ddcf3cc5747bc247dd70c80e0c2fa123ae0ec5a
DarisTarga <daristarga@gmail.com>
2025-10-22 00:23:12 +0300

    zadacha.js

---
+++ task 13-14/zadacha.js
@@ -0,0 +1,42 @@
+function verify() {
+    console.log("a, b, c")
+    let a = parseInt(elementA.value);
+    let b = parseInt(elementB.value);
+    let c = parseInt(elementC.value);
+    let d = parseInt(elementD.value);
+    console.log(a, b, c, d)
+
+
+if (( a < c && b < d) || (a < d && b < c)) {
+    result = "Вписывается"
+        check = true;
+}
+
+else {
+        result = "Не вписывается"
+        check = false;
+    }
+    document.getElementById("result").value = result;
+}
+
+function send() {
+    if (check) {
+        document.getElementById("UserEnter").submit();
+    } else {
+        alert("Есть недостатки. Повторите ввод")
+    }
+}
+
+let result;
+let check;
+
+const elementA = document.getElementById("a");
+const elementB = document.getElementById("b");
+const elementC = document.getElementById("c");
+const elementD = document.getElementById("d");
+
+const elementVerify = document.getElementById("verify");
+elementVerify.addEventListener('click', verify);
+
+const elementSend = document.getElementById("send");
+elementSend.addEventListener('click', send)
